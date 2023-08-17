c53a5c4845fb5d975909e0360ac6f5f075cbcd84
qalib01 <qalib.mmmdli@gmail.com>
2023-08-17 08:00:14 +0400

    Introduction to state

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -2,13 +2,51 @@ import React, { Component } from "react";
 import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from "reactstrap";
 
 export default class CategoryList extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            categories: [
+                {
+                    id: 1,
+                    name: 'Rose',
+                },
+                {
+                    id: 2,
+                    name: 'Tulip',
+                },
+                {
+                    id: 3,
+                    name: 'Lily',
+                },
+                {
+                    id: 4,
+                    name: 'Peony',
+                },
+                {
+                    id: 5,
+                    name: 'Orchid',
+                },
+                {
+                    id: 6,
+                    name: 'Marigold',
+                },
+            ],
+        };
+    };
+
     render() {
         return (
             <React.Fragment>
                 <h3> { this.props.info.title } </h3>
-                <ListGroup numbered flush horizontal>
-                    <ListGroupItem>Cras justo odio</ListGroupItem>
-                    <ListGroupItem active>Dapibus ac facilisis in</ListGroupItem>
+                <ListGroup flush horizontal>
+                    {
+                        this.state.categories.map(category => (
+                            <ListGroupItem key={ category.id }> { category.name } </ListGroupItem>
+                        ))
+                    }
+
+                    {/* <ListGroupItem active>Dapibus ac facilisis in</ListGroupItem>
                     <ListGroupItem disabled>Morbi leo risus</ListGroupItem>
                     <ListGroupItem color="success">Porta ac consectetur ac</ListGroupItem>
                     <ListGroupItem>Vestibulum at eros
@@ -18,7 +56,7 @@ export default class CategoryList extends Component {
                         <ListGroupItemText>
                             Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.
                         </ListGroupItemText>
-                    </ListGroupItem>
+                    </ListGroupItem> */}
                 </ListGroup>
             </React.Fragment>
         );
